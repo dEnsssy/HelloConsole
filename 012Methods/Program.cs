03cbb7239bac045b9a454bc96a8fe7c9374325b4
dEnsssy <kravchuk.n.o@gmail.com>
2023-04-23 15:13:14 +0300

    12+13

--- 012Methods/Program.cs
+++ 012Methods/Program.cs
@@ -129,7 +129,7 @@ void SelectionSort(int[] array)
 
       for (int j = i + 1; j < array.Length; j++)
       {
-         if (array[j] < array[minPosition]) minPosition = j;
+         if (array[j] > array[minPosition]) minPosition = j;
       }
 
       int temporary = array[i];
